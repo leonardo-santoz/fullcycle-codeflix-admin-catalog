43b218e534b4fcd3b9341aa5ef1525aeeeaa9c2e
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-10-20 21:54:50 +0000

    feat: category entity - creating category entity

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,34 @@
+export type CategoryConstructorProps = {
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+};
+
+export class Category {
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id; //TODO: generate UUID if not provided
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
+
+  //this create is a factory method
+  static create(props: CategoryCreateCommand): Category {
+    return new Category(props);
+  }
+}
