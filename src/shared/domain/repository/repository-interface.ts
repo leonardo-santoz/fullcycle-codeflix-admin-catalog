ed37f184d58bd642b3f3d46edcb1c7919279827f
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-11-13 20:43:55 -0300

    feat: category repository - add  abstraction for in-memory testing

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -4,10 +4,10 @@ import { ValueObject } from "../value-object";
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>;
   bulkInsert(entities: E[]): Promise<void>;
-  update(entity: E[]): Promise<void>;
-  delete(entity_id: E): Promise<void>;
+  update(entity: E): Promise<void>;
+  delete(entity_id: EntityId): Promise<void>;
 
-  findById(entity: EntityId): Promise<E>;
+  findById(entity_id: EntityId): Promise<E | null>;
   findAll(): Promise<E[]>;
 
   getEntity(): new (...args: any[]) => E;
