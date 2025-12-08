3d6b8f666965e042bc2adc0dcf94433e04916ea2
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-12-08 09:46:21 -0300

    feat: category repository - add custom search method in memory repo

--- src/shared/infra/db/in-memory/in-memory.repository.ts
+++ src/shared/infra/db/in-memory/in-memory.repository.ts
@@ -1,6 +1,14 @@
 import { Entity } from "../../../domain/entity";
 import { NotFoundError } from "../../../domain/errors/not-found-error";
-import { IRepository } from "../../../domain/repository/repository-interface";
+import {
+  IRepository,
+  ISearchableRepository,
+} from "../../../domain/repository/repository-interface";
+import {
+  SearchParams,
+  SortDirection,
+} from "../../../domain/repository/search-params";
+import { SearchResult } from "../../../domain/repository/search-result";
 import { ValueObject } from "../../../domain/value-object";
 
 export abstract class InMemoryRepository<
@@ -52,4 +60,77 @@ export abstract class InMemoryRepository<
   }
 
   abstract getEntity(): new (...args: any[]) => E;
+}
+
+export abstract class InMemorySearchableRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    Filter = string
+  >
+  extends InMemoryRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>
+{
+  sortableFields: string[] = [];
+  async search(props: SearchParams<Filter>): Promise<SearchResult<E>> {
+    const filteredItems = await this.applyFilter(this.items, props.filter);
+    const sortedItems = this.applySort(
+      filteredItems,
+      props.sort,
+      props.sort_dir
+    );
+    const paginatedItems = this.applyPaginate(
+      sortedItems,
+      props.page,
+      props.per_page
+    );
+
+    return new SearchResult({
+      items: paginatedItems,
+      total: filteredItems.length,
+      current_page: props.page,
+      per_page: props.per_page,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: Filter | null
+  ): Promise<E[]>;
+
+  protected applyPaginate(
+    items: E[],
+    page: SearchParams["page"],
+    per_page: SearchParams["per_page"]
+  ) {
+    const start = (page - 1) * per_page; //1 * 15 = 15
+    const limit = start + per_page; //15 + 15 = 30
+    return items.slice(start, limit); //15,30
+  }
+
+  protected applySort(
+    items: E[],
+    sort: string | null,
+    sort_dir: SortDirection | null,
+    custom_getter?: (sort: string, item: E) => any
+  ) {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items;
+    }
+
+    return [...items].sort((a, b) => {
+      //@ts-ignore
+      const aValue = custom_getter ? custom_getter(sort, a) : a[sort];
+      //@ts-ignore
+      const bValue = custom_getter ? custom_getter(sort, b) : b[sort];
+      if (aValue < bValue) {
+        return sort_dir === "asc" ? -1 : 1;
+      }
+
+      if (aValue > bValue) {
+        return sort_dir === "asc" ? 1 : -1;
+      }
+
+      return 0;
+    });
+  }
 }
